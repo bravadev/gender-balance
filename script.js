e581dd9b951dcb7199bc3e4fd43716d58099b569
Tugdual <harel.tugdual@gmail.com>
2022-12-06 13:52:48 +0100

    Revert "update"
    This reverts commit 48e2a13567eb7cce7448daeb5768745d4a66e65b.

--- script.js
+++ script.js
@@ -1,69 +1,33 @@
-const API_KEY = "VOTRE_CLE_API"; // Remplacer "VOTRE_CLE_API" par votre propre clé d'API
-const POPULATION_API_URL = "https://api.data.unicef.org/v2/population/countries/all/indicators/SP.POP.TOTL?api_key=" + API_KEY;
-const GIRLS_POPULATION_API_URL = "https://api.data.unicef.org/v2/population/countries/all/indicators/SP.POP.GIRLS.IN?api_key=" + API_KEY;
-const BOYS_POPULATION_API_URL = "https://api.data.unicef.org/v2/population/countries/all/indicators/SP.POP.BOYS.IN?api_key=" + API_KEY;
+//const API_URL = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json";
+const API_URL = "https://api.data.unicef.org/v2/population/countries/all/indicators/SP.POP.TOTL";
 
 let chart = null;
 
 // Fonction pour récupérer les données sur l'équilibre filles/garçons dans le monde
 const getData = async () => {
+// Utiliser la méthode ".catch()" pour gérer les erreurs potentielles dans la promesse
+    return fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
 // Récupération des données sur la population totale de chaque pays
-    const populationData = await fetch(POPULATION_API_URL)
-        .then((response) => response.json())
-        .then((data) => data.data)
-        .catch((error) => {
-            console.error("Erreur lors de la récupération des données sur la population totale : ", error);
-        });
+            const populationData = data[1];
 
-// Récupération des données sur la population des filles de chaque pays
-    const girlsPopulationData = await fetch(GIRLS_POPULATION_API_URL)
-        .then((response) => response.json())
-        .then((data) => data.data)
-        .catch((error) => {
-            console.error("Erreur lors de la récupération des données sur la population des filles : ", error);
-        });
-
-// Récupération des données sur la population des garçons de chaque pays
-    const boysPopulationData = await fetch(BOYS_POPULATION_API_URL)
-        .then((response) => response.json())
-.then((data) => data.data)
-        .catch((error) => {
-            console.error("Erreur lors de la récupération des données sur la population des garçons : ", error);
-        });
-
-// Calcul du nombre total de filles et de garçons dans le monde
-    let totalGirls = 0;
-    let totalBoys = 0;
-    populationData.forEach((country) => {
-        totalGirls += country.value * 0.5; // Hypothèse que la population est composée de 50% de filles et de 50% de garçons
-        totalBoys += country.value * 0.5;
-    });
-
-// Calcul de la différence de population entre les filles et les garçons pour chaque pays
-    const countryData = [];
-    populationData.forEach((populationCountry) => {
-        girlsPopulationData.forEach((girlsCountry) => {
-            boysPopulationData.forEach((boysCountry) => {
-                if (populationCountry.country_code === girlsCountry.country_code && populationCountry.country_code === boysCountry.country_code) {
-                    const girls = girlsCountry.value;
-                    const boys = boysCountry.value;
-                    const diff = girls - boys;
-                    countryData.push({
-                        country: populationCountry.country_name,
-                        girls: girls,
-                        boys: boys,
-                        diff: diff
-                    });
-                }
+            // Calcul du nombre total de filles et de garçons dans le monde
+            let totalGirls = 0;
+            let totalBoys = 0;
+            populationData.forEach((country) => {
+                totalGirls += country.value * 0.5; // Hypothèse que la population est composée de 50% de filles et de 50% de garçons
+                totalBoys += country.value * 0.5;
             });
-        });
-    });
 
-    return {
-        totalGirls: totalGirls,
-        totalBoys: totalBoys,
-        countryData: countryData
-    };
+            return {
+                girls: totalGirls,
+                boys: totalBoys
+            };
+        })
+        .catch((error) => {
+            console.error("Erreur lors de la récupération des données : ", error);
+        });
 
 }
 
@@ -79,13 +43,24 @@ const createChart = (data) => {
                 labels: ["Filles", "Garçons"],
                 datasets: [{
                     label: "Population",
-                    data: [data.totalGirls, data.totalBoys],
-                    backgroundColor: ["#f67280", "#c06c84"]
+                    data: [data.girls, data.boys],
+                    backgroundColor: ["#f8b195", "#f67280"]
                 }]
             },
             options: {
                 legend: {
-                    display: false
+                    display: true,
+                    position: "bottom"
+                },
+                legendCallback: function(chart){
+                    const legendHtml = [];
+                    legendHtml.push('<ul class="' + chart.id + '-legend">');
+                    for (var i = 0; i < chart.data.datasets.length; i++) {
+                        legendHtml.push('<li><span style="background-color:' + chart.data.datasets[i].backgroundColor[0] + '"></span>Filles : ' + data.girls + '</li>');
+                        legendHtml.push('<li><span style="background-color:' + chart.data.datasets[i].backgroundColor[1] + '"></span>Garçons : ' + data.boys + '</li>');
+                    }
+                    legendHtml.push('</ul>');
+                    return legendHtml.join("");
                 }
             }
         });
@@ -94,22 +69,6 @@ const createChart = (data) => {
         if (chart !== null) {
             updateChartType();
         }
-    }
-
-// Affichage des données sur les différences de population entre les filles et les garçons pour chaque pays
-    if (data.countryData.length > 0) {
-        const countryTable = document.getElementById("country-data");
-
-        // Entête du tableau HTML
-        let countryTableHTML = "<thead><tr><th>Pays</th><th>Filles</th><th>Garçons</th><th>Différence</th></tr></thead><tbody>";
-
-        // Lignes du tableau HTML
-        data.countryData.forEach((country) => {
-            countryTableHTML += "<tr><td>" + country.country + "</td><td>" + country.girls + "</td><td>" + country.boys + "</td><td>" + country.diff + "</td></tr>";
-        });
-        countryTableHTML += "</tbody>";
-
-        countryTable.innerHTML = countryTableHTML;
     }
 
 }
